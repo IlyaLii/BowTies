b960de4589034e1f4b8c97e30de8d9ac46448002
Ilya <agronat88@icloud.com>
2020-01-29 21:34:55 +0300

    added new info and ui

--- BowTies/ViewController.swift
+++ BowTies/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var timesWornLabel: UILabel!
+    @IBOutlet weak var lastWornLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    @IBOutlet weak var wearButton: UIButton!
+    @IBOutlet weak var rateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func segmentControl(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func wear(_ sender: UIButton) {
+    }
+    
+    @IBAction func rate(_ sender: UIButton) {
+    }
 }
 
