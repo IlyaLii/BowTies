7088a42b24a8af3ee0d75c5e5bac306145a4cc18
Ilya <agronat88@icloud.com>
2020-01-30 18:33:23 +0300

    added first subclass of entity

---
+++ BowTies/BowTieModel/BowTie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BowTie+CoreDataClass.swift
+//  BowTies
+//
+//  Created by Li on 1/30/20.
+//  Copyright © 2020 Li. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BowTie)
+public class BowTie: NSManagedObject {
+
+}
